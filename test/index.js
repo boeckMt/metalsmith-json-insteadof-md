8387d22dd0aa63177445b37340b35fa95d67d9c0
Mathias Boeck <Mathias.Boeck@dlr.de>
2020-08-07 11:16:07 +0200

    update dependencies and test

--- test/index.js
+++ test/index.js
@@ -6,36 +6,36 @@ var Metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
 var jsonContent = require('..');
 
-describe('metalsmith-json-insteadof-md', function() {
-  it('should convert json files', function(done) {
+describe('metalsmith-json-insteadof-md', function () {
+  it('should convert json files', function (done) {
     Metalsmith('test/fixtures/basic')
       .use(jsonContent())
-      .build(function(err) {
+      .build(function (err) {
         if (err) return done(err);
         equal('test/fixtures/basic/expected', 'test/fixtures/basic/build');
         done();
       });
   });
 
-  it('should allow a "keys" option', function(done) {
+  it('should allow a "keys" option', function (done) {
     Metalsmith('test/fixtures/keys')
       .use(jsonContent())
-      .build(function(err, files) {
+      .build(function (err, files) {
         if (err) return done(err);
         assert.equal('A Title for the JSON Post', files['index.html'].titel);
         done();
       });
   });
 
-  it('should allow zu use it with metalsmith-markdown', function(done) {
+  it('should allow zu use it with metalsmith-markdown', function (done) {
     Metalsmith('test/fixtures/withmarkdown')
       .use(
         jsonContent({
-          fileExtnameTo: '.md'
+          fileExtnameTo: '.md',
         })
       )
       .use(markdown())
-      .build(function(err, files) {
+      .build(function (err, files) {
         if (err) return done(err);
         assert.equal('A Title for the JSON Post', files['index.html'].titel);
         done();
